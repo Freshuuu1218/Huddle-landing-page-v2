7c4f3a44042f5fbd4a2314f11df5ad25830f9b30
Kacper Klimczak <kacper_klimczak_2609@protonmail.com>
2023-03-03 01:20:54 +0100

    add script (invalid email error)

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const sectionTop2 = document.querySelector('#sectionTop2')
 const sectionBottom1 = document.querySelector('#sectionBottom1')
 const sectionBottom2 = document.querySelector('#sectionBottom2')
 const footerPattern = document.querySelector('#footer-pattern')
-
+const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1-3}\.[0-9]{1-3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const subscribe = document.querySelector('#subscribe')
+const input = document.querySelector('#input');
+const error = document.querySelector('#error');
 window.addEventListener('resize', ()=>{
     if(window.innerWidth <768){
         sectionTop1.setAttribute('src','./images/bg-section-top-mobile-1.svg')
@@ -17,5 +20,13 @@ window.addEventListener('resize', ()=>{
         sectionBottom1.setAttribute('src','./images/bg-section-bottom-desktop-1.svg')
         sectionBottom2.setAttribute('src','./images/bg-section-bottom-desktop-2.svg')
         footerPattern.setAttribute('src','./images/bg-footer-top-desktop.svg')
+    }
+})
+subscribe.addEventListener('click', ()=>{
+
+        if(re.test(input.value)){
+        error.style.display = 'none';
+    }else{
+        error.style.display = 'block';
     }
 })
